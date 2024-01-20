c869e02f211174637c7b095dcf2b02bc45942d51
Trent Miller <113061760+BlizzardRAZE@users.noreply.github.com>
2024-01-20 13:03:42 -0500

    Finished Business Information

--- Week_2/BusinessInformation/App.js
+++ Week_2/BusinessInformation/App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image, Linking } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='dark'/>
+      <SafeAreaView style= {styles.container}>
+        <View style={styles.imageContainer}>
+            <Image style={styles.image} source={require("./assets/images/restaurant.jpg")}/>
+        </View>
+        <View style={styles.informationContainer}>
+          <Text style={styles.text}>Oscar's Food & Spirits</Text>
+          <Text style={styles.text} onPress={() => Linking.openURL("https://oscarsnmb.com/")}>www.oscarsnmb.com</Text>
+          <Text style={styles.text} onPress={() => Linking.openURL("tel:(843)-272-0707")}> (843)-272-0707</Text>
+          <Text style={styles.text} onPress={() => Linking.openURL("https://maps.app.goo.gl/5eQfJXNKpSsJMa3EA")}>  Open in Google Maps</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eba134',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer:{
+    flex: 3,
+    paddingTop: 50,
+    width: "100%",
+  },
+  image:{
+    height:300,
+    width: "100%",
+    resizeMode: "cover",
+    borderWidth: 6,
+    borderColor: "#6e0202",
+  },
+  informationContainer:{
+    flex: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text:{
+    fontSize: 25,
+    fontStyle: "italic",
+    color: "#ffffff",
+    marginBottom: 10,
   },
 });
