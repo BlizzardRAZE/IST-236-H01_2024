d8499ef27cfbed80d6217fbb5d455ab7ef8a0acd
Trent Miller <trmiller2003@gmail.com>
2024-02-28 11:41:12 -0500

    Initial Commit

---
+++ Week_8/BlackJack/screens/GameOverScreen.js
@@ -0,0 +1,93 @@
+import { View, StyleSheet, Text } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Title from "../components/Title";
+import NavButton from "../components/NavButton";
+import Header from "../components/header";
+import Colors from "../constants/colors";
+import { LinearGradient } from "expo-linear-gradient";
+
+function GameOverScreen(props) {
+  // Set Safe Area Screen Boundaries
+  const insets = useSafeAreaInsets();
+
+  const playerScore = props.user;
+  const computerScore = props.computer;
+
+  let titleText = <Title>It's a Tie</Title>;
+
+  if (
+    (playerScore <= 21 && playerScore > computerScore) ||
+    (playerScore <= 21 && computerScore > 21)
+  ) {
+    titleText = <Title>Player Wins!</Title>;
+  }
+  if (
+    (computerScore <= 21 && computerScore > playerScore) ||
+    (computerScore <= 21 && playerScore > 21)
+  ) {
+    titleText = <Title>Computer Wins!</Title>;
+  }
+
+  return (
+    <LinearGradient
+      colors={[Colors.accent200, Colors.accent800, Colors.accent200]}
+      style={styles.rootContainer}
+    >
+      <View
+        style={[
+          styles.rootContainer,
+          {
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            paddingLeft: insets.left,
+            paddingRight: insets.right,
+          },
+        ]}
+      >
+        <View style={styles.titleContainer}>{titleText}</View>
+
+        <View style={styles.scoreContainer}>
+          <Header>Computer Score:</Header>
+          <Text style={styles.scoreText}>{computerScore}</Text>
+        </View>
+
+        <View style={styles.scoreContainer}>
+          <Header>Player Score:</Header>
+          <Text style={styles.scoreText}>{playerScore}</Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <NavButton onPress={props.onNext}>Play Now!</NavButton>
+        </View>
+      </View>
+    </LinearGradient>
+  );
+}
+
+export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+  },
+  titleContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  scoreContainer: {
+    flex: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  scoreText: {
+    fontSize: 50,
+    color: Colors.primary500,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
